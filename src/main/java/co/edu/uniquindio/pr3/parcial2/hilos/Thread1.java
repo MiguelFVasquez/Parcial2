a6e35d220369f70e3eda27dd7478866eefd1a271
MiguelFVasquez <126723056+MiguelFVasquez@users.noreply.github.com>
2023-10-25 16:13:02 -0500

    Hilos

---
+++ src/main/java/co/edu/uniquindio/pr3/parcial2/hilos/Thread1.java
@@ -0,0 +1,7 @@
+package co.edu.uniquindio.pr3.parcial2.hilos;
+
+public class Thread1 extends Thread{
+
+
+
+}
---
+++ src/main/java/co/edu/uniquindio/pr3/parcial2/hilos/Thread2.java
@@ -0,0 +1,8 @@
+package co.edu.uniquindio.pr3.parcial2.hilos;
+
+public class Thread2 extends Thread{
+
+
+
+
+}
